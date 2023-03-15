0d8dfd5cd8f54c7616e644cdf6f91882db20a6f4
Dmitriy Shipilov <shipilenok1@gmail.com>
2023-03-15 13:06:03 +0300

    Add Title, Genre and Category -viewsets, touch api/urls.py, add SimpleRouter and router.urls in app/urls.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+
+class TitleViewSet(ModelViewSet):
+    pass
+
+
+class CategoryViewSet(ModelViewSet):
+    pass
+
+
+class GenreViewSet(ModelViewSet):
+    pass
